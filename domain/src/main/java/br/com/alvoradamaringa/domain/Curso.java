2e1eae9b2e8bbd47ba16ad8a35385b0cf423e7be
AndersonGrossi <andersongrossisantos@gmail.com>
2015-05-03 18:34:13 -0300

    Update Curso.java

--- domain/src/main/java/br/com/alvoradamaringa/domain/Curso.java
+++ domain/src/main/java/br/com/alvoradamaringa/domain/Curso.java
@@ -3,4 +3,20 @@ package br.com.alvoradamaringa.domain;
 public class Curso {
      private long idCurso;
      private String descricao;   
+
+     public void setId(Integer  idcurso){
+          this.idcurso=idcurso;
+          
+     }
+     public Integer getid(Integer idcurso){
+          return this.idcurso;
+     }
+     public void setdescricao(String descricao){
+          this.descricao=descricao;
+     }
+     public String getDescricao(String descricao){
+          return this.descricao;
+     }
 }
+
+
