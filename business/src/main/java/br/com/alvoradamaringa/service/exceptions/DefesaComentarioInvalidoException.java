d9ab53bf6ebe146d1e9f000ebffb237143e28da2
Marcelo Beani da Costa Junior <marcelo666junior666@gmail.com>
2015-06-18 23:42:47 -0300

    Create DefesaComentarioInvalidoException.java

---
+++ business/src/main/java/br/com/alvoradamaringa/service/exceptions/DefesaComentarioInvalidoException.java
@@ -0,0 +1,10 @@
+package br.com.alvoradamaringa.service.spec;
+
+public class DefesaComentarioInvalidoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
