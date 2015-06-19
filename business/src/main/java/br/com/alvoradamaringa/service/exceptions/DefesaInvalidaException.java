7ec1ce3ef5e8477fa3dbc27ba54e643301999190
Marcelo Beani da Costa Junior <marcelo666junior666@gmail.com>
2015-06-18 23:43:58 -0300

    Update and rename LoginInvalidoException.java to DefesaInvalidaException.java

---
+++ business/src/main/java/br/com/alvoradamaringa/service/exceptions/DefesaInvalidaException.java
@@ -0,0 +1,12 @@
+package br.com.alvoradamaringa.service.exceptions;
+
+public class DefesaInvalidaException extends Exception {
+
+	/**
+	 * 
+	 */
+	
+	private static final long serialVersionUID = 1L;
+	
+
+}
